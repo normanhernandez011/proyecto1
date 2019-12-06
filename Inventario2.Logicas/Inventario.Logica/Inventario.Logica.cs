d89960c18417f79ad9f10644bd178d50ecc41d6e
normanhernandez011 <57654592+normanhernandez011@users.noreply.github.com>
2019-12-05 20:12:51 -0600

    Add files via upload

---
+++ Inventario2.Logicas/Inventario.Logica/Inventario.Logica.cs
@@ -0,0 +1,21 @@
+﻿using InventarioTI2.Data.InventarioPrueba;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario2.Logicas.Inventario.Logica
+{
+     public class Inventario
+    {
+         
+        //ImventarioPrueba datosSql = new ImventarioPrueba();
+        //public List<InventarioPrueba> ListaInventario2()
+        //{
+        //    return datosSql.ListaInventario2();
+        //}
+
+     
+    }
+}
---
+++ conexion/Seguridad/SeguridadDatoSql.cs
@@ -0,0 +1,44 @@
+﻿using conexion.conexion.datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static InventarioTIModelo.Seguridad.SeguridadModel;
+
+namespace conexion.Seguridad
+{
+   public  class SeguridadDatoSql
+    {
+        //BD_INV_ACTIVOSEntities
+        public Usuario GetUsuario(string Usuario, string Pass)
+        {
+            try
+            {
+                using(var db = new BD_INV_ACTIVOSEntities())
+                {
+                    var oUser = (from d in db.USUARIO where d.USUARIO1 == Usuario.Trim() && d.CONTRASENIA == Pass.Trim()
+                                 select d).FirstOrDefault();
+                    Usuario Usuarioid = new Usuario
+                    {
+                        Id = oUser.ID,
+                        User = oUser.USUARIO1,
+                        //Estado = oUser.ESTADO,
+                        Contraseña = oUser.CONTRASENIA
+
+                    };
+               
+                    return Usuarioid;
+                }
+              
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+                throw;
+            }
+
+        }
+
+    }
+}
